fcfe9b1dd8503a95b60fafbbad8f01c4a0d31e2d
fabrevitor <vitor_fabre@hotmail.com>
2021-06-23 16:04:20 -0300

    Delegation Examples

--- src/main/kotlin/Delegation.kt
+++ src/main/kotlin/Delegation.kt
@@ -1,6 +1,54 @@
-// O que é delegation? keyword by?
+import kotlin.properties.Delegates
+
+// DELEGATION BY -> CLASSES
+
+interface Base {
+    fun printMe() //abstract method
+}
+class BaseImpl(val x: Int) : Base {
+    override fun printMe() { println(x) }   //implementation of the method
+}
+class Derived(b: Base) : Base by b  // delegating the public method on the object b
+
+// DELEGATION BY - PROPERTIES
+
+class StudentHeavy{
+    init{
+        println("Student Heavy initialized.........")
+    }
+}
+
+class Student{
+    val heavy by lazy {
+        StudentHeavy()
+    }
+
+    var marks:Int by Delegates.observable(50){
+            property, oldValue, newValue -> println("$property -> Old value: $oldValue -- New value: $newValue ")
+    }
+
+    var age: Int by Delegates.vetoable(24){property, oldValue, newValue ->
+        println("$property -> Old value: $oldValue -- New value: $newValue ")
+        newValue >= 18
+    }
+}
+
+
 
 fun main() {
 
+    // CLASS
+    val test = BaseImpl(10)
+    Derived(test).printMe() // prints 10 :: accessing the printMe() method
 
+    // PROPERTY
+    val student = Student()
+
+    student.heavy // só é instanciado a StudentHeavy quando usamos a val heavy de Student, por conta do Lazy!
+
+    student.marks = 70 // observable, quand é alterado, o observable executa o lambda.
+
+    student.age = 13
+    println("Age Student: ${student.age}")
 }
+
